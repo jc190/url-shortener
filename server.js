68a5b0e2382d79e5684b0c647f09ccd343553130
James <jim.calhoun@outlook.com>
2016-08-12 04:56:25 +0000

    fixed bug

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var path = require('path');
 var shortid = require('shortid');
 
 var express = require('express');
@@ -8,6 +9,8 @@ var mongodb = require('mongodb').MongoClient;
 var dbURL = 'mongodb://' + process.env.IP + ':27017/shorturl';
 
 var server = http.createServer(app);
+
+app.use(express.static(path.resolve(__dirname, 'client')));
 
 app.get('/new/*', function(req, res) {
     var obj = {};
@@ -39,45 +42,23 @@ app.get('/new/*', function(req, res) {
     res.end(JSON.stringify(obj));
 })
 
-// app.get(/^\/new\/((http|https)(:\/\/www\.|:\/\/)(.*)(\.)(.*))/i, function(req, res) {
-//     var urls = {};
-//     var uid = shortid.generate();
-//     urls.original = req.params[0];
-//     urls.new = 'https://urls-jimcalhoun.c9users.io/' + uid;
-//     res.end(JSON.stringify(urls));
-//     mongodb.connect(dbURL, function(err, db) {
-//         if (err) {
-//             return console.log(err);
-//         }
-//         console.log('Connected to DB');
-//         var collection = db.collection('urls');
-//         collection.insert({
-//             uid: uid,
-//             url: urls.original
-//         }, function(err, data) {
-//             if (err) {
-//                 return console.log(err);
-//             }
-//         });
-//         db.close();
-//     });
-// });
-
 app.get('/:id', function(req, res) {
-    mongodb.connect(dbURL, function(err, db) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('Connected to DB');
-        var collection = db.collection('urls');
-        collection.find({uid: req.params.id}).toArray(function(err, data) {
+    if (req.params.id != 'favicon.ico') {
+        mongodb.connect(dbURL, function(err, db) {
             if (err) {
                 return console.log(err);
             }
-            res.redirect(data[0].url);
+            console.log('Connected to DB');
+            var collection = db.collection('urls');
+            collection.find({uid: req.params.id}).toArray(function(err, data) {
+                if (err) {
+                    return console.log(err);
+                }
+                res.redirect(data[0].url);
+            });
+            db.close();
         });
-        db.close();
-    });
+    }
 });
 
 server.listen(8080, process.env.IP, function() {
